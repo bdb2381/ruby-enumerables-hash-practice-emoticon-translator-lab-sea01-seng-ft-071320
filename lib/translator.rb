aa949b524427019bc78efab5ac980588b8b0a1a9
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 17:41:22 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,12 +4,12 @@ require 'yaml'
 
 def load_library(file_path)
 
-  file_as_hash = YAML.load_file(file_path)    #load the file
-    hash_of_meaning = {}
-    file_as_hash.each do |name,arrays|
-        hash_of_meaning[name] = {}   #||
-        hash_of_meaning[name][:english] = arrays[0]
-        hash_of_meaning[name][:japanese] = arrays[1]
+  file_as_hash = YAML.load_file(file_path)    #load the file automatically as hash, with keys and values in an array form
+    hash_of_meaning = {}                      #initialize blank hash. This is will hold the final data in the format we want
+    file_as_hash.each do |emotion_name_key,arrays|        #for every item in the file, do the following
+        hash_of_meaning[emotion_name_key] = {}            #create a nested hash for each emotion name
+        hash_of_meaning[emotion_name_key][:english] = arrays[0]
+        hash_of_meaning[emotion_name_key][:japanese] = arrays[1]
 
     end
   hash_of_meaning
