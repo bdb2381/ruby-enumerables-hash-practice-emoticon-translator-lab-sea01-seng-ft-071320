a5ff2760e43bf71c2c6978d6b59d24c415663b06
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 15:03:47 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,9 +4,9 @@ require 'yml'
 
 def load_library(emoticons)
     file_data = File.read(emoticons)  #load the data
-    file_data.collect do |index|
-      puts index
-    end
+    #file_data.collect do |index|
+    #  puts index
+    #end
 
 #   binding.pry
 #return a hash with {'emoticon_name' => { :english => "value", :japanese => "value"} } format
