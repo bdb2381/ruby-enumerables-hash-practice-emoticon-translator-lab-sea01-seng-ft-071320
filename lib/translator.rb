bc2ecb8bde84d35058a5462c5b88de5cf432f089
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 19:42:11 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,9 +22,9 @@ def get_english_meaning(file_path, japanese_emoticon_to_translate  )
 file_as_hash = load_library(file_path)    #load the file automatically as hash, with keys and values in an array form
   #hash_of_meaning = {}
 #  binding.pry
-file_as_hash.each do |key, value|
+file_as_hash.each do |key, nested_key_value|
   #  value.each do |language_key, emoticon_value|
-          if value[:japanese] == japanese_emoticon_to_translate
+          if nested_key_value[:japanese] == japanese_emoticon_to_translate
                  return key
           end  #end of if
 binding.pry
