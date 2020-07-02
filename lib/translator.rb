0c9e8121bc7bd2560a0546488ac861cc58d9244b
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 19:04:48 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ file_as_hash = load_library(file_path)    #load the file automatically as hash, 
   #hash_of_meaning = {}
 #  binding.pry
 file_as_hash.each do |key, value|
-    value.each do |language_key, emoticon_value|
+    value.map do |language_key, emoticon_value|
           if value[:japanese] == japanese_emoticon_to_translate
               puts "I'm in the if!"
               puts value[:japanese]
