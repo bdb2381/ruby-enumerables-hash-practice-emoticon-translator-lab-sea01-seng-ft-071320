224a5d9b233d8b2aab4d0a1d4aa18f01381be1cb
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 17:51:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,14 +7,12 @@ def load_library(file_path)
   file_as_hash = YAML.load_file(file_path)    #load the file automatically as hash, with keys and values in an array form
     hash_of_meaning = {}                      #initialize blank hash. This is will hold the final data in the format we want
     file_as_hash.each do |emotion_name_key,arrays_of_emoticons|        #for every item in the file, do the following
-        hash_of_meaning[emotion_name_key] = {}            #create a nested hash for each emotion name
-        hash_of_meaning[emotion_name_key][:english] = arrays_of_emoticons[0]
-        hash_of_meaning[emotion_name_key][:japanese] = arrays_of_emoticons[1]
+        hash_of_meaning[emotion_name_key] = {}            #create a nested hash for each emotion name, based on the emotion name_key
+        hash_of_meaning[emotion_name_key][:english] = arrays_of_emoticons[0]    #for any data in index 0, put it here
+        hash_of_meaning[emotion_name_key][:japanese] = arrays_of_emoticons[1]   #for any data in index 1, put it here
 binding.pry
     end
-  hash_of_meaning
-#binding.pry
-#return a hash with {'emoticon_name' => { :english => "value", :japanese => "value"} } format
+  hash_of_meaning    #return a hash with all .yml data in {'emoticon_name' => { :english => "value", :japanese => "value"} } format
 end
 
 
