d1c161673a22dd421309970a1498cf101e6af6d8
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 15:42:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,12 +3,24 @@ require 'pry'
 
 
 def load_library(emoticons)
+=begin
     data = []
     file_data = File.read(emoticons)  #load the data
     data =  file_data.split(/-/)
     hash = Hash[data.collect { |item| [item]}]
 binding.pry
 puts data
+=end
+  hash = {}
+  File.open(emoticons) do |fp|
+    fp.each do |line|
+      key, value = line.chomp.split("-")
+      has[key] = value
+    end
+  end
+
+
+
 
 #   binding.pry
 #return a hash with {'emoticon_name' => { :english => "value", :japanese => "value"} } format
