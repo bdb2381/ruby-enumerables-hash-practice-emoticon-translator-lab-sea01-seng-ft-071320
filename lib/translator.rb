a205d4a7e5420cd61654240747425ec7939a6fa3
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 19:27:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ file_as_hash.each do |key, value|
               return key
 
           else
-            return "Sorry, that emotion was not found" #value[:japanese] != japanese_emoticon_to_translate
+             "Sorry, that emotion was not found" #value[:japanese] != japanese_emoticon_to_translate
           #  return "Sorry, that emotion was not found"
 binding.pry
 
