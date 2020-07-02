73581d7db101b217fdc95acc30b5f2c4c294a8b9
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 18:27:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,9 @@ file_as_hash.each do |key, value|
     value.each do |language_key, emoticon_value|
       if emoticon_value[1] = japanese_emoticon_to_translate
         binding.pry
+        
       end
+      binding.pry
     end
 end
 #binding.pry
