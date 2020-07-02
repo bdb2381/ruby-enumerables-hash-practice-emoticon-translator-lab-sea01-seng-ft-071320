2ae4a95d005d42d66d9e6b825b880b288ac22aba
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 18:43:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,7 +24,7 @@ file_as_hash = load_library(file_path)    #load the file automatically as hash, 
 file_as_hash.each do |key, value|
     value.each do |language_key, emoticon_value|
       if emoticon_value != japanese_emoticon_to_translate
-          puts "Emoticon Value is #{emoticon_value} and input var is #{japanese_emoticon_to_translate}"
+          puts "Emoticon Value is #{emoticon_value}, name is #{key} and input var is #{japanese_emoticon_to_translate}"
 
 
     elsif emoticon_value == japanese_emoticon_to_translate
