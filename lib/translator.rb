c8caf5b84f52dafed7e98ce29721df29aa5744cf
Brad Beggs <brad.beggs@gmail.com>
2020-07-02 16:18:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -10,19 +10,25 @@ def load_library(emoticons)
     hash = Hash[data.collect { |item| [item]}]
 binding.pry
 puts data
-=end
+
   hash = {"emotion_meaning" => {}, "the_emoticon" => {}}
   File.open(emoticons) do |file|
     file.map do |line|
-      key, value = line.chomp.split("-")
-      hash["emotion_meaning"][key] = value
+      #key, value = line.chomp.split("-")
+      hash["emotion_meaning"][][key] = value
     #  binding.pry
     puts hash
     #$puts value
     end
     #binding.pry
   end
-
+=end
+hash = {"get_meaning" =>{}, "get_emoticon" =>{}}
+YAML.load_file(file_path).each do |meaning, language_array|
+  hash["get_emoticon"][language_array[0]] = language_array[1]
+  hash["get_meaning"][language_array[1]] = meaning
+end
+hash
 
 
 
